9917e42a540921204728e86e81be98cf3cc3b640
Ninh <le.van.ninh@sun-asterisk.com>
2021-03-20 13:48:06 +0700

    Adding dev instance

--- variables.tf
+++ variables.tf
@@ -10,3 +10,7 @@ variable "db_instance_class" {}
 variable "dbname" {}
 variable "dbuser" {}
 variable "dbpassword" {}
+variable "dev_instance_type" {}
+variable "dev_ami" {}
+variable "key_name" {}
+variable "public_key_path" {}
